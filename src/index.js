5ab361666fad6dbce08bc8bbdf5097f52bb90137
Ruturaj <ruturajmengal2016@gmail.com>
2023-03-19 09:30:35 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Form from './Form';
 import './index.css';
 import App from './App' 
-import Text from './Text' 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App /> 
-    <Text />
-    <Form /> 
+    <h1>Hello World</h1> 
   </React.StrictMode>
 );
