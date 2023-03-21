ffa7218a0b60f62f1ebff8e77a7731f8448b6ce3
Ruturaj <ruturajmengal2016@gmail.com>
2023-03-21 19:01:00 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React from "react";
-import { Routes, Route,Link } from "react-router-dom";
-import About from "./About";
-import Home from "./Home";
+import React from 'react'
 
 function App() {
   return (
-    <>
-    <nav>
-      <ul>
-        <li><Link to="/home">Home</Link></li> 
-        <li><Link to="/about">About</Link></li> 
-      </ul>
-    </nav>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </>
-  );
+    <div className='App'>
+      
+    </div>
+  )
 }
 
-export default App;
+
+function Nav(){
+  return(
+    
+  )
+}
+
+export default App
