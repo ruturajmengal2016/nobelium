1f15a6123032bac81303eb7da33f83384dc26efc
Ruturaj <ruturajmengal2016@gmail.com>
2023-03-21 17:58:16 +0530

    commit

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='flex justify-center items-start bg-blue-400'>
+        <h1>About</h1>
+    </div>
+  )
+}
+
+export default About
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='flex justify-center items-start bg-red-400'>
+        <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
