88ff66ad44bf7b366a8941dd361e128a71692be2
Ruturaj <ruturajmengal2016@gmail.com>
2023-03-27 14:56:04 +0530

    commit

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="h-screen flex justify-center items-center bg-[url('https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171__480.jpg')] bg-no-repeat bg-cover">
+    <div className="h-screen flex justify-center items-center bg-[url('https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171__480.jpg')] bg-no-repeat bg-cover overflow-hidden">
       <span className="sm:text-[10rem] w-fit text-center font-mono font-bold text-red-400 animate-pulse text-[3rem] h-fit">
         <span className="text-blue-500 italic">Nobelium</span> Web Development
         cohort
