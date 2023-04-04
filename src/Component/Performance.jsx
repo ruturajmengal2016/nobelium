f1f469b89f24ba25969f85c8e84efb783a61e1e3
Ruturaj <ruturajmengal2016@gmail.com>
2023-04-04 11:02:13 +0530

    commit

--- src/Component/Performance.jsx
+++ src/Component/Performance.jsx
@@ -5,29 +5,24 @@ function Performance() {
   const [data, setData] = useState([]);
   const location = useLocation();
   const url = `https://nobelium-0vvw.onrender.com/user/${location.state.id}`;
-  const fetchData = async () => {
+  const fetchData = async (url) => {
     const res = await fetch(url);
     const user = await res.json();
     setData(user);
   };
 
   useEffect(() => {
-    fetchData();
-  }, []);
-  console.log(data);
+    fetchData(url);
+  }, [url]);
   return (
-    <div className="h-screen bg-sky-300">
-      {data.map((element) => {
-        return (
-          <div key={element.Roll_no}>
-            {element.Roll_no === location.state.id && (
-              <>
-                <div>{element.Name}</div>
-              </>
-            )}
-          </div>
-        );
-      })}
+    <div className="h-screen bg-sky-300 flex justify-center items-center">
+      <div className="bg-purple-900 h-[90%] w-[90%] flex p-4 gap-3 justify-evenly">
+        <div className="h-[10rem] w-[10rem] bg-red-600 flex-grow"></div>
+        <div className="h-[10rem] w-[10rem] bg-red-600 flex-grow"></div>
+        <div className="h-[10rem] w-[10rem] bg-red-600 flex-grow"></div>
+        <div className="h-[10rem] w-[10rem] bg-red-600 flex-grow"></div>
+        <div className="h-[10rem] w-[10rem] bg-red-600 flex-grow"></div>
+      </div>
     </div>
   );
 }
