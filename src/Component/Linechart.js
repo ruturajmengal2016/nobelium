4ff729f49287f4ea04669f43d41748c74d6a3572
Ruturaj <ruturajmengal2016@gmail.com>
2023-04-08 23:55:58 +0530

    commit

---
+++ src/Component/Linechart.js
@@ -0,0 +1,49 @@
+import React, { useMemo } from "react";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+function Linechart({ user }) {
+  const data = [];
+  const totalCodeZinger = useMemo(() => {
+    const regxMonth = new RegExp("CodeZinger", "gi");
+    return Object.keys(user).filter((ele) => {
+      return regxMonth.test(ele);
+    });
+  }, [user]);
+  let mko = totalCodeZinger
+    .map((ele) => {
+      return ele.split(" ");
+    })
+    .map((ele, ind) => {
+      return ele;
+    });
+  for (let i in mko) {
+    data.push({
+      date: mko[i][0] + " " + mko[i][1],
+      value: user[totalCodeZinger[i]],
+    });
+  }
+  return (
+    <>
+      <LineChart width={1000} height={300} data={data}>
+        <Line
+          type="monotone"
+          dataKey="value"
+          stroke="#2979ff"
+          strokeWidth={3}
+        />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="date" stroke="#ff5722" />
+        <YAxis stroke="#ff5722" />
+        <Tooltip />
+      </LineChart>
+    </>
+  );
+}
+
+export default Linechart;
--- src/Component/Performance.jsx
+++ src/Component/Performance.jsx
@@ -1,23 +1,127 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "../Axios/axios";
-
+import Box from "@mui/material/Box";
+import { grey, red, indigo } from "@mui/material/colors";
+import Linechart from "./Linechart";
 function Performance() {
   const [user, setData] = useState([]);
   const location = useLocation();
-  const fetchData = async () => {
-    const res = await axios.get(`/user/${location.state.id}`);
+  const fetchData = async (id) => {
+    const res = await axios.get(`/user/${id}`);
     setData(res.data);
   };
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData(location.state.id);
+  }, [location.state.id]);
+
+  const totalAttendance = useMemo(() => {
+    const regxAttendance = new RegExp("Attendance", "gi");
+    return Object.keys(user)
+      .filter((ele) => {
+        return regxAttendance.test(ele);
+      })
+      .map((ele) => {
+        return user[ele];
+      })
+      .reduce((acc, curr) => acc + curr, 0);
+  }, [user]);
+  const totalCodeZinger = useMemo(() => {
+    const regxCodezinger = new RegExp("CodeZinger", "gi");
+    return Object.keys(user)
+      .filter((ele) => {
+        return regxCodezinger.test(ele);
+      })
+      .map((ele) => {
+        return user[ele];
+      })
+      .reduce((acc, curr) => acc + curr, 0);
+  }, [user]);
+
+  const totalAssignment = useMemo(() => {
+    const regxAssignment = new RegExp("Submission", "gi");
+    return Object.keys(user)
+      .filter((ele) => {
+        return regxAssignment.test(ele);
+      })
+      .map((ele) => {
+        return user[ele];
+      })
+      .reduce((acc, curr) => acc + curr, 0);
+  }, [user]);
   return (
-    <div className="h-screen bg-sky-300 flex justify-center items-center">
-      <div className="bg-purple-900 h-[90%] w-[90%] grid grid-flow-col p-3">
-        <div className="bg-red-500 h-[15rem] w-[50rem]"></div>
-        <div className="bg-red-500 h-[15rem] w-[20rem]"></div>
-      </div>
+    <div className="h-screen flex justify-center items-center">
+      <Box
+        sx={{
+          width: "90vw",
+          height: "90vh",
+          backgroundColor: grey[900],
+          display: "flex",
+          flexDirection: "column",
+          padding: "1rem",
+          gap: "1rem",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            width: "90%",
+            justifyContent: "space-between",
+            alignSelf:"center"
+          }}
+        >
+          <Box
+            sx={{
+              backgroundColor: red["A200"],
+              width: "20rem",
+              display: "flex",
+              justifyContent: "space-between",
+              flexWrap: "wrap",
+              padding: "0.55rem",
+              maxHeight: "10rem",
+              fontSize: "1.2rem",
+              fontWeight: "bold",
+              color: "white",
+            }}
+          >
+            <span>{user["Student's Name"]}</span>
+            <span>{user["Roll No."]}</span>
+            <span>{user["Email Id"]}</span>
+            <span>{user["Mobile No."]}</span>
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: indigo["A200"],
+              width: "20rem",
+              height: "10rem",
+              padding: "1rem",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              fontWeight:"bold",
+              color:"white"
+            }}
+          >
+            <span>Total Attendance: {totalAttendance}</span>
+            <span>Total Code Zinger Marks: {totalCodeZinger}</span>
+            <span>Total Assignments: {totalAssignment}</span>
+          </Box>
+        </Box>
+        <Box sx={{ padding: "1rem",display:"flex",justifyContent:"center" }}>
+          <Box
+            sx={{
+              backgroundColor:"#b3e5fc",
+              width: "95%",
+              height: "25rem",
+              display:"flex",
+              justifyContent:"center",
+              alignItems:"center"
+            }}
+          >
+            <Linechart user={user} />
+          </Box>
+        </Box>
+      </Box>
     </div>
   );
 }
